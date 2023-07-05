a0351759b32dbcf60fbf4ed6ad8de9b94b782231
punnreaykhy <96402600+punnreaykhy@users.noreply.github.com>
2023-07-05 20:39:34 +0700

    update
    + landing page
    + product page
    + cart pop-up

---
+++ src/views/LandingView.vue
@@ -0,0 +1,135 @@
+<script setup>
+    import NavBar from '../components/NavBar.vue';
+    import Footer from '../components/Footer.vue';
+</script>
+
+<template>
+    <main>
+        <NavBar />
+        <!-- <heroBanner /> -->
+        <div
+            class="heroBanner p-md-2 mx-md-3 rounded-4 d-flex ps-5 pb-5 flex-column justify-content-center text-center p-3"
+            style="height: 780px"
+            id="heroCover"
+        >
+            <div
+                class="content d-flex flex-column gap-2 text-white align-items-center"
+            >
+                <h1 class="w-75">
+                    Welcome to KPR Revolutionizing Electronic Store
+                </h1>
+                <p class="fs-5 w-75">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    dolore ma. Aliquet bibendum enim facili neque convallis,
+                    consectetur adipiscing elit.
+                </p>
+                <div>
+                    <button class="btn rounded-3">Learn More</button>
+                </div>
+            </div>
+        </div>
+
+        <h2 class="text-center p-4">
+            Limited Time Offer - up to 70% Off Your Purchase!
+        </h2>
+
+        <div class="d-flex px-3 mb-4 gap-4 justify-content-around bg-white m-auto rounded-2 p-3" style="width:90%;">
+            <img
+                src=""
+                alt=""
+                class="border border-5 border-danger align-self-center"
+                style="height:10rem; width:10rem"
+            />
+            <div class="w-75">
+                <h4>GoPro HERO6 4K Action Camera - Black</h4>
+                <h3>$923</h3>
+                <div class="d-flex align-items-center">
+                    <div class="stared">
+                        <i class="bi bi-star-fill"></i
+                        ><i class="bi bi-star-fill"></i
+                        ><i class="bi bi-star-fill"></i
+                        ><i class="bi bi-star-fill"></i>
+                    </div>
+                    <i class="bi bi-star"></i>
+                    <span class="ms-2 rated">7.5</span>
+                    <div class="dot"></div>
+                    <span class="ms-2">154 orders</span>
+                    <div class="dot"></div>
+                    <span class="ms-2 text-success">Free Shipping</span>
+                </div>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit </p>
+                <div class=" text-primary">View details</div>
+            </div>
+            <div>
+              <i type="button" class="bi bi-cart3 me-2 border px-2 py-1 rounded"></i>
+            </div>
+        </div>
+
+        <div class="social mb-3 d-flex flex-column text-center w-100 bg-white p-4">
+          <h3>Stay connected</h3>
+          <div class="d-flex gap-4 m-auto">
+            <div id="ig"></div>
+            <div id="fb"></div>
+            <div id="red"></div>
+            <div id="tw"></div>
+            <div id="yt"></div>
+          </div>
+        </div>
+
+        <div class="dlApp d-flex w-100 justify-content-center gap-5 mb-3">
+          <div id="ios"></div>
+          <div id="ad"></div>
+        </div>
+
+        <Footer />
+    </main>
+</template>
+
+<style scoped>
+    @import url('../assets/style.css');
+    .heroBanner {
+        background-image: url('../assets/darkBanner.svg');
+        background-repeat: no-repeat;
+        background-size: 100% auto;
+    }
+
+    .social div div{
+      width: 100px;
+      height: 101px;
+      background-size: 100% auto;
+    }
+    #ig{
+      background-image: url('../assets/social_Icon/ig.svg');
+    }
+    #fb{
+      background-image: url('../assets/social_Icon/fb.svg');
+    }
+    #red{
+      background-image: url('../assets/social_Icon/red.svg');
+    }
+    #tw{
+      background-image: url('../assets/social_Icon/twiter.svg');
+    }
+    #yt{
+      background-image: url('../assets/social_Icon/yt.svg');
+    }
+
+    .dlApp div{
+      width: 171px;
+      height: 58px;
+      background-size: 100% auto;
+    }
+    #ios{
+      background-image: url('../assets/dl_app/ios.svg');
+    }
+    #ad{
+      background-image: url('../assets/dl_app/ad.svg');
+    }
+    .dot {
+        background: #000000;
+        width: 5px;
+        height: 5px;
+        border-radius: 100%;
+        margin-left: 8px;
+    }
+</style>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,256 @@
+<script setup>
+    import NavBar from '../components/NavBar.vue';
+    import Footer from '../components/Footer.vue';
+</script>
+<script>
+    export default {
+        // components: { Button },
+        data() {
+            return {
+                categories: [],
+            };
+        },
+        methods: {
+            increaseValue() {
+                var value = parseInt(
+                    document.getElementById('number').value,
+                    10
+                );
+                value = isNaN(value) ? 0 : value;
+                value++;
+                document.getElementById('number').value = value;
+            },
+
+            decreaseValue() {
+                var value = parseInt(
+                    document.getElementById('number').value,
+                    10
+                );
+                value = isNaN(value) ? 0 : value;
+                value < 1 ? (value = 1) : '';
+                value--;
+                document.getElementById('number').value = value;
+            },
+        },
+        async mounted() {},
+    };
+</script>
+
+<template>
+    <main>
+        <NavBar />
+
+        <div class="route-link d-flex gap-2 m-4">
+            <a href="#">Home</a>
+            <a href="#">Category</a>
+            <a href="#">Product</a>
+        </div>
+
+        <div class="product bg-white mx-4 p-4">
+            <div class="d-flex gap-3">
+                <div
+                    class="p-img border"
+                    style="width: 500px; height: 500px"
+                ></div>
+                <div class="p-detail w-50">
+                    <div class="text-success">In stock</div>
+                    <h4>MacBook Air Case 2022 with Touch ID Air 13.6 Cover</h4>
+                    <div class="d-flex align-items-center">
+                        <div class="stared">
+                            <i class="bi bi-star-fill"></i
+                            ><i class="bi bi-star-fill"></i
+                            ><i class="bi bi-star-fill"></i
+                            ><i class="bi bi-star-fill"></i>
+                        </div>
+                        <i class="bi bi-star"></i>
+                        <span class="ms-2 rated">7.5</span>
+                        <div class="dot"></div>
+                        <span class="ms-2">154 orders</span>
+                        <div class="dot"></div>
+                        <span class="ms-2 text-success">Free Shipping</span>
+                    </div>
+                    <div
+                        class="bg-info d-flex text-center justify-content-around"
+                    >
+                        <div>
+                            <h5>$99</h5>
+                            <span>50-100 pcs</span>
+                        </div>
+                        <div
+                            class="bg-black my-2"
+                            style="width: 1px"
+                        ></div>
+                        <div>
+                            <h5>$99</h5>
+                            <span>50-100 pcs</span>
+                        </div>
+                        <div
+                            class="bg-black my-2"
+                            style="width: 1px"
+                        ></div>
+                        <div>
+                            <h5>$99</h5>
+                            <span>50-100 pcs</span>
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="d-flex gap-5">
+                            <label for="price">Price:</label>
+                            <div>Negotiable</div>
+                        </div>
+                        <hr />
+                        <div class="d-flex gap-5">
+                            <label for="price">Price:</label>
+                            <div>Negotiable</div>
+                        </div>
+                        <div class="d-flex gap-5">
+                            <label for="price">Price:</label>
+                            <div>Negotiable</div>
+                        </div>
+                        <div class="d-flex gap-5">
+                            <label for="price">Price:</label>
+                            <div>Negotiable</div>
+                        </div>
+                        <hr />
+                        <div class="d-flex gap-5">
+                            <label for="price">Price:</label>
+                            <div>Negotiable</div>
+                        </div>
+                        <div class="d-flex gap-5">
+                            <label for="price">Price:</label>
+                            <div>Negotiable</div>
+                        </div>
+                        <div class="d-flex gap-5">
+                            <label for="price">Price:</label>
+                            <div>Negotiable</div>
+                        </div>
+                        <hr />
+                    </div>
+
+                    <div class="d-flex border rounded-3 overflow-hidden " style="width:fit-content">
+                        <div
+                        class="value-button d-flex justify-content-center align-items-center"
+                            id="decrease"
+                            @click="decreaseValue"
+                            value="Decrease Value"
+                        >
+                            -
+                        </div>
+                        <input
+                            type="number"
+                            id="number"
+                            value="0"
+                        />
+                        <div
+                            class="value-button d-flex justify-content-center align-items-center"
+                            id="increase"
+                            @click="increaseValue"
+                            value="Increase Value"
+                        >
+                            +
+                        </div>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex mt-3">
+                <div class="sub-img"></div>
+            </div>
+
+            <div class="related-p p-2 border m-3">
+                <h4>Related products</h4>
+                <div class="d-flex gap-2">
+                    <div class="card p-3">
+                        <div class="relate-p-img"></div>
+                        <h5 class="mt-2">Phone</h5>
+                        <span>$234-$347</span>
+                    </div>
+                    <div class="card p-3">
+                        <div class="relate-p-img"></div>
+                        <h5 class="mt-2">Phone</h5>
+                        <span>$234-$347</span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <Footer />
+    </main>
+</template>
+
+<style scoped>
+
+    .sub-img{
+        width: 100px;
+        height: 100px;
+        border: solid 1px;
+        border-radius: 1em;
+    }
+
+    .relate-p-img{
+        width: 150px;
+        height: 150px;
+        border: solid 1px;
+        border-radius: 1em;
+    }
+    .dot {
+        background: #000000;
+        width: 5px;
+        height: 5px;
+        border-radius: 100%;
+        margin-left: 8px;
+    }
+
+    .value-button {
+        display: inline-block;
+        border: 1px solid #ddd;
+        margin: 0px;
+        width: 40px;
+        height: 40px;
+        text-align: center;
+        vertical-align: middle;
+        background: #eee;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    .value-button:hover {
+        cursor: pointer;
+    }
+
+    form #decrease {
+        margin-right: -4px;
+        border-radius: 8px 0 0 8px;
+    }
+
+    form #increase {
+        margin-left: -4px;
+        border-radius: 0 8px 8px 0;
+    }
+
+    form #input-wrap {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    input#number {
+        text-align: center;
+        border: none;
+        border-top: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+        margin: 0px;
+        width: 40px;
+        height: 40px;
+    }
+
+    input[type='number']::-webkit-inner-spin-button,
+    input[type='number']::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+</style>
